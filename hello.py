b62c577bd3d5f7db1c1dbd97a73e643d391d91d6
Adrián <la.muerte.de.pandora@gmail.com>
2018-09-20 22:26:13 +0200

    añadiendo el archivo hello

---
+++ hello.py
@@ -0,0 +1,9 @@
+numero_a_adivinar = 5
+
+numero_del_usuario = int(input("adivina el numero: "))
+
+if numero_a_adivinar == numero_del_usuario:
+    print("Has ganado")
+
+else 
+    print("Has perdido")
