779f6ec1589b67e97f1fe2c4dc518342aa268da2
Adrián <la.muerte.de.pandora@gmail.com>
2018-09-21 22:34:24 +0200

    puñ..programa de adivinar el jodid.. numero con la funcion if y else

--- hello.py
+++ hello.py
@@ -1,24 +1,26 @@
-numero_a_adivinar = 7
+numero_a_adivinar = 4
 
-numero_del_usuario = int(input("adivina el numero: "))
+numero_del_usuario = int(input("numero a adivinar: "))
 
-if numero_a_adivinar == numero_del_usuario:
-    print("adivinaaste")
-else:
-    numero_del_usuario = int(input("vuelve a intentarlo: "))
+
 
 if numero_del_usuario == numero_a_adivinar:
-    print("adivinaaste")
+    print("Acertaste")
 
 else:
-    numero_del_usuario = int(input("ultimo uintento: "))
+    numero_del_usuario = int(input("prueba otra vez: "))
 
-if numero_del_usuario ==  numero_a_adivinar:
-    print("adivinaaste")
+if numero_del_usuario == numero_a_adivinar:
+    print("Acertaste")
 
 else:
-    print("A la mierda")
+    numero_del_usuario = int(input("ultimo intento: "))
 
+if numero_a_adivinar == numero_del_usuario:
+    print("Acertaste")
+
+else:
+    print("A cagar")
 
 
 
